b6df4a90cdc254f0c0505cba2e93da6b5b823378
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-03 13:43:10 +0600

    related product size fixed

--- src/app/components/main/product-view/related-product/related-product.ts
+++ src/app/components/main/product-view/related-product/related-product.ts
@@ -23,10 +23,10 @@ export class RelatedProduct {
   }
 
   breakpoints = {
-    480: { slidesPerView: 2, spaceBetween: 5 },
+    480: { slidesPerView: 2, spaceBetween: 15 },
     768: { slidesPerView: 3, spaceBetween: 10 },
-    1024: { slidesPerView: 4, spaceBetween: 15 },
-    1440: { slidesPerView: 5, spaceBetween: 20 }
+    1024: { slidesPerView: 3, spaceBetween: 20 },
+    1440: { slidesPerView: 4, spaceBetween: 20 }
     // 480: { slidesPerView: 1, spaceBetween: 5 },
     // 768: { slidesPerView: 2, spaceBetween: 10 },
     // 1024: { slidesPerView: 3, spaceBetween: 20 },
