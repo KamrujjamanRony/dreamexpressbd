638d16a68d0a8bbf188b3f9491c406d69109982f
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-05 16:01:19 +0600

    feature-product img optimization done

--- src/app/components/main/home/feature-product/feature-product.ts
+++ src/app/components/main/home/feature-product/feature-product.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BdtPipe } from '../../../../pipes/bdt.pipe';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-feature-product',
-  imports: [BdtPipe],
+  imports: [BdtPipe, NgOptimizedImage],
   templateUrl: './feature-product.html',
   styleUrl: './feature-product.css',
 })
