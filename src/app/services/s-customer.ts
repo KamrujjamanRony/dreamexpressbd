e7ed2e39527a482c858605bb58b02b372ef5881a
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-11 12:15:23 +0600

    customer service added done

--- src/app/services/s-customer.ts
+++ src/app/services/s-customer.ts
@@ -2,34 +2,35 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { CustomerM } from '../models/Customer';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SCustomer {
   private readonly http = inject(HttpClient);
-  private apiUrl = `${environment.apiUrl}/ECommerceUser`;
+  private apiUrl = `${environment.apiUrl}/CustomerLogIn`;
 
-  add(model: any): Observable<any> {
-    return this.http.post(this.apiUrl, model,
-      { responseType: 'text' })
+  add(model: CustomerM): Observable<CustomerM> {
+    return this.http.post<CustomerM>(this.apiUrl, model,
+      { responseType: 'text' as 'json' })
   }
 
-  search(): Observable<any> {
-    return this.http.get<any[]>(`${this.apiUrl}`)
+  search(): Observable<CustomerM[]> {
+    return this.http.get<CustomerM[]>(`${this.apiUrl}/Search`)
   }
 
-  get(id: any): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  get(id: number): Observable<CustomerM> {
+    return this.http.get<CustomerM>(`${this.apiUrl}/${id}`);
   }
 
-  update(id: any, updateRequest: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, updateRequest,
-      { responseType: 'text' });
+  update(id: number, updateRequest: CustomerM): Observable<CustomerM> {
+    return this.http.put<CustomerM>(`${this.apiUrl}/${id}`, updateRequest,
+      { responseType: 'text' as 'json' });
   }
 
-  delete(id: any): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  delete(id: number): Observable<CustomerM> {
+    return this.http.delete<CustomerM>(`${this.apiUrl}/${id}`);
   }
   
 }
