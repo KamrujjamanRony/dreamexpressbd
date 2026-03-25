06d22b20167699c63ef589a1cd0b993ceca0525f
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-25 17:03:12 +0600

    feat: add CommonModule import to product wrapper component and display category-wise products as JSON

--- src/app/components/main/home/product-wrapper/product-wrapper.ts
+++ src/app/components/main/home/product-wrapper/product-wrapper.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { ProductCard } from '../../../shared/product-card/product-card';
 import { ProductSkeleton } from "../../../shared/product-skeleton/product-skeleton";
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-wrapper',
-  imports: [ProductCard, ProductSkeleton, RouterLink],
+  imports: [ProductCard, ProductSkeleton, RouterLink, CommonModule],
   templateUrl: './product-wrapper.html',
   styleUrl: './product-wrapper.css',
 })
