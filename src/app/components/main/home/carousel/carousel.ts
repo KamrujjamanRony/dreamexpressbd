1e650b3272a362f764ca6d061c5dafa5ef5a364b
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-25 15:01:22 +0600

    feat: update carousel component to use dynamic image API for background images

--- src/app/components/main/home/carousel/carousel.ts
+++ src/app/components/main/home/carousel/carousel.ts
@@ -1,14 +1,16 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-carousel',
   imports: [RouterLink],
   templateUrl: './carousel.html',
   styleUrl: './carousel.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Carousel {  
     carousels = input<any[]>([]);
+    imageApi = environment.ImageApi;
 
 }
