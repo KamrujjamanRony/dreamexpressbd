34763ff8c872cb0f534f69d1fed17573d6a96434
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-26 19:20:03 +0600

    feat: add app initializer to configure guest service on application startup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideAppInitializer } from '@angular/core';
 import { provideRouter, RouteReuseStrategy, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { authInterceptor } from './interceptor/auth-interceptor';
 import { CustomReuseStrategy } from './utils/route-reuse.strategy';
+import { SGuest } from './services/s-guest';
+import { firstValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +14,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withHashLocation()),
     { provide: RouteReuseStrategy, useClass: CustomReuseStrategy },
     provideHttpClient(withInterceptors([authInterceptor])),
-    CookieService
+    CookieService,
+    provideAppInitializer(() => {
+      const guestService = inject(SGuest);
+      return firstValueFrom(guestService.initialize());
+    }),
   ]
 };
