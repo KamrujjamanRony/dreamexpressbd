4f7404007cb2a3d0993b0021f33324eb29c32b2b
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-26 19:16:34 +0600

    feat: remove unnecessary JSON display of categoryWiseProducts for cleaner UI

--- src/app/components/main/home/product-wrapper/product-wrapper.ts
+++ src/app/components/main/home/product-wrapper/product-wrapper.ts
@@ -2,11 +2,10 @@ import { Component, input } from '@angular/core';
 import { ProductCard } from '../../../shared/product-card/product-card';
 import { ProductSkeleton } from "../../../shared/product-skeleton/product-skeleton";
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-wrapper',
-  imports: [ProductCard, ProductSkeleton, RouterLink, CommonModule],
+  imports: [ProductCard, ProductSkeleton, RouterLink],
   templateUrl: './product-wrapper.html',
   styleUrl: './product-wrapper.css',
 })
