bc5f571d855e18d2a2ff03993edbba2590eee6aa
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-29 20:17:41 +0600

    fix(category-list): enhance error handling in submission and deletion methods

--- src/app/components/admin/category-list/category-list.ts
+++ src/app/components/admin/category-list/category-list.ts
@@ -158,8 +158,8 @@ export class CategoryList {
       },
       error: (error) => {
         this.isSubmitted.set(false);
-        console.error(error?.message || error?.error?.message || 'An error occurred during submission.');
-        this.toast.danger('Saved unsuccessful!', 'bottom-left', 3000);
+        console.error('Error:', error);
+        this.toast.danger(error?.error || 'Saved unsuccessful!', 'bottom-left', 3000);
       }
     });
   }
@@ -219,7 +219,7 @@ export class CategoryList {
           this.toast.success('item deleted successfully!', 'bottom-right', 5000);
         },
         error: (error) => {
-          this.toast.danger('item deleted unsuccessful!', 'bottom-left', 3000);
+          this.toast.danger(error?.error || 'item deleted unsuccessful!', 'bottom-left', 3000);
           console.error('Error deleting item:', error);
         }
       });
