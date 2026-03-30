85dd548ebba4bbdf8f28a860cd468e5bb6f73c33
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-30 15:42:05 +0600

    feat(loader): implement route loading indicator with animations and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal, DestroyRef } from '@angular/core';
+import { Router, RouterOutlet, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { environment } from '../environments/environment';
 import { Toast } from './utils/toast/toast';
 import { Confirm } from "./utils/confirm/confirm";
@@ -14,9 +15,19 @@ import { SSeo } from './services/s-seo';
 })
 export class App implements OnInit {
   private seo = inject(SSeo);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
   protected readonly title = signal(environment.companyName);
+  protected readonly routeLoading = signal(false);
 
   ngOnInit() {
     this.seo.init();
+    this.router.events.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.routeLoading.set(true);
+      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.routeLoading.set(false);
+      }
+    });
   }
 }
