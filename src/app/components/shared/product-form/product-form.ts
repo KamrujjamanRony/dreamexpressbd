5f668c33f562c1efee0af3e4770d64ef672d3352
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-03-31 15:38:15 +0600

    feat(product-form): enhance Quill editor toolbar with additional header options for improved formatting

--- src/app/components/shared/product-form/product-form.ts
+++ src/app/components/shared/product-form/product-form.ts
@@ -44,8 +44,16 @@ export class ProductForm {
   quillModules = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'header': [1, 2, 3, false] }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'align': [] }],
+      ['blockquote', 'code-block'],
+      ['link'],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'direction': 'rtl' }],
       ['clean']
     ]
   };
