3e6f33a86a1c038ee167c2095ce1c12391318bc5
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-04-01 06:06:27 +0600

    feat(about): refactor SAbout service methods to use observables for improved data handling

--- src/app/services/s-about.ts
+++ src/app/services/s-about.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { AboutUsM } from '../models/AboutUs';
 
 @Injectable({
@@ -11,20 +11,33 @@ export class SAbout {
   private readonly http = inject(HttpClient);
   private apiUrl = `${environment.apiUrl}/AboutUs`;
 
-  add(model: FormData): Observable<AboutUsM> {
-    return this.http.post<AboutUsM>(this.apiUrl, model)
+  add(model: Partial<AboutUsM>): Observable<AboutUsM> {
+    return this.http.post<any>(this.apiUrl, model).pipe(
+      map(res => this.normalize(res))
+    );
   }
 
   get(id: any): Observable<AboutUsM> {
-    return this.http.get<AboutUsM>(`${this.apiUrl}/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`).pipe(
+      map(res => this.normalize(res))
+    );
   }
 
-  update(id: any, updateRequest: FormData): Observable<AboutUsM> {
-    return this.http.put<AboutUsM>(`${this.apiUrl}/${id}`, updateRequest);
+  update(id: any, model: Partial<AboutUsM>): Observable<AboutUsM> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, model).pipe(
+      map(res => this.normalize(res))
+    );
   }
 
   delete(id: any): Observable<AboutUsM> {
     return this.http.delete<AboutUsM>(`${this.apiUrl}/${id}`);
   }
-  
+
+  private normalize(res: any): AboutUsM {
+    const data = res?.data || res;
+    return {
+      ...data,
+      info: Array.isArray(data.info) ? data.info : data.info?.$values || [],
+    };
+  }
 }
