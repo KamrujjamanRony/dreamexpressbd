1421d8a9043485b3c2bf18db40a9da97980a89da
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-04-01 06:06:14 +0600

    feat(about): simplify AboutUsM interface by removing redundant fields and introducing AboutUsInfoM for better structure

--- src/app/models/AboutUs.ts
+++ src/app/models/AboutUs.ts
@@ -1,22 +1,14 @@
+export interface AboutUsInfoM {
+    id?: number;
+    title: string;
+    desc: string;
+    imgU: string;
+}
+
 export interface AboutUsM {
     id?: number;
     companyID: number;
     heading: string;
-    title: string;
-    description: string;
-    title2?: string;
-    description2?: string;
-    title3?: string;
-    description3?: string;
-    title4?: string;
-    description4?: string;
-    title5?: string;
-    description5?: string;
-    title6?: string;
-    description6?: string;
-    title7?: string;
-    description7?: string;
-    title8?: string;
-    description8?: string;
-    imageUrl?: string;
+    imageUrl: string;
+    info: AboutUsInfoM[];
 }
