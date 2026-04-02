43000976d504ac1e084c0582bdf4eb4992ddc8e6
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-04-02 16:51:11 +0600

    feat: update editorial image styles for better responsiveness and adjust image API URL for local development

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -5,7 +5,7 @@ export const environment = {
     whatsappNumber: '1818545586',
     messengerUsername: 'md.kamrujjaman.822234',
     apiUrl: 'https://api.dreamexpressbd.com/p',
-    ImageApi: 'https://api.dreamexpressbd.com/uploads/',
+    ImageApi: 'http://localhost:1005/uploads/',  //https://api.dreamexpressbd.com/uploads/
     webUrl: 'https://dreamexpressbd.com',
     siteDescription: 'Dream Express BD - Your trusted online shop for quality medical equipment, healthcare products, and diagnostic devices in Bangladesh. Fast delivery, secure payment, and great deals.',
     emptyImg: 'https://www.mykite.in/kb/NoImageFound.jpg.png',
